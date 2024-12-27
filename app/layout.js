5c71816675f71d23c6e80230f9d80553b0c63f6c
Marc-Ruizenaar <info@marcruizenaar.com>
2024-12-27 23:10:22 +0100

    Added Analytics componant

--- app/layout.js
+++ app/layout.js
@@ -16,6 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={ManRope.className}>
       <body>{children}</body>
+      <Analytics />
     </html>
   );
 }
